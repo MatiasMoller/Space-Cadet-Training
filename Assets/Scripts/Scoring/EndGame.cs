6a2db1d899d323751703ad5d36972f195bf2386d
MatHartley_HomePC <115046381+MatHartley@users.noreply.github.com>
2023-10-05 19:20:11 +1100

    05/10/23
    Highscore Table accepting time and user input name
    Table not capped, not data validation
    Needs to have data cleared

--- Assets/Scripts/Scoring/EndGame.cs
+++ Assets/Scripts/Scoring/EndGame.cs
@@ -7,30 +7,56 @@ namespace MathewHartley
 {
     public class EndGame : MonoBehaviour
     {
+        [Header("Highscore Objects")]
         public GameObject scoreTable;
-        public GameObject inputWindow;
+        public HighScoreTable highscoreTable;
+
+        [Header("Timer Objects")]
         public TextMeshProUGUI finalTimeText;
-        public SceneLoader endGame;
         public GameTimer gameTimer;
+        private float playerTime;
+
+        [Header("Player Input")]
+        private string playerName;
+        public GameObject inputWindow;
+        public TMP_InputField playerInput;
+
+        [Header("Managers")]
+        public SceneLoader endGame;
+
         private void OnTriggerEnter(Collider other)
         {
+            //Round the timer to 2 decimal places
+            playerTime = Mathf.Round(gameTimer.currentTime * 100f) /100f;
+
             inputWindow.SetActive(true);
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
             if (gameTimer.hasFormat)
             {
-                finalTimeText.text = gameTimer.currentTime.ToString(gameTimer.timeFormats[gameTimer.format]);
-                Debug.Log("Run Time: " + gameTimer.currentTime.ToString(gameTimer.timeFormats[gameTimer.format]));
+                finalTimeText.text = playerTime.ToString(gameTimer.timeFormats[gameTimer.format]);
+                Debug.Log("Run Time: " + playerTime.ToString(gameTimer.timeFormats[gameTimer.format]));
             }
             else
             {
-                finalTimeText.text = gameTimer.currentTime.ToString();
-                Debug.Log("Run Time: " + gameTimer.ToString());
+                finalTimeText.text = playerTime.ToString();
+                Debug.Log("Run Time: " + playerTime.ToString());
             }
         }
 
+        public void SubmitScore()
+        {
+            playerName = playerInput.text;
+            highscoreTable.AddHighscoreEntry(playerTime, playerName);
+            StartCoroutine(ShowScores());
+        }
+
+
         IEnumerator ShowScores()
         {
+            inputWindow.SetActive(false);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
             scoreTable.SetActive(true);
             Debug.Log("Started Coroutine at timestamp : " + Time.time);
             yield return new WaitForSeconds(10);
