1eebe661138fbdafec661b3e504d58384ee52794
MatHartley_HomePC <115046381+MatHartley@users.noreply.github.com>
2023-10-05 18:18:41 +1100

    05/10/23
    UI for user input

--- Assets/Scripts/Scoring/EndGame.cs
+++ Assets/Scripts/Scoring/EndGame.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace MathewHartley
 {
     public class EndGame : MonoBehaviour
     {
         public GameObject scoreTable;
+        public GameObject inputWindow;
+        public TextMeshProUGUI finalTimeText;
         public SceneLoader endGame;
+        public GameTimer gameTimer;
         private void OnTriggerEnter(Collider other)
         {
-            scoreTable.SetActive(true);
-            StartCoroutine(ReadScore());
+            inputWindow.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            if (gameTimer.hasFormat)
+            {
+                finalTimeText.text = gameTimer.currentTime.ToString(gameTimer.timeFormats[gameTimer.format]);
+                Debug.Log("Run Time: " + gameTimer.currentTime.ToString(gameTimer.timeFormats[gameTimer.format]));
+            }
+            else
+            {
+                finalTimeText.text = gameTimer.currentTime.ToString();
+                Debug.Log("Run Time: " + gameTimer.ToString());
+            }
         }
 
-        IEnumerator ReadScore()
+        IEnumerator ShowScores()
         {
+            scoreTable.SetActive(true);
             Debug.Log("Started Coroutine at timestamp : " + Time.time);
             yield return new WaitForSeconds(10);
             Debug.Log("Finished Coroutine at timestamp : " + Time.time);
