f075782b1f3eaae954bd5e29854eba1d8f562da3
Moller_Matt_Laptop <136556009+MatiasMoller@users.noreply.github.com>
2023-10-12 17:59:45 +1100

    arrow fixed

--- Assets/Scripts/Arrow/ArrowChange.cs
+++ Assets/Scripts/Arrow/ArrowChange.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowChange : MonoBehaviour
@@ -9,11 +7,18 @@ public class ArrowChange : MonoBehaviour
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Gate2"))
-        {  
-            arrowIndicator.RotateTowardsGoal2();
-            arrowIndicator.playerEnteredTrigger = true;
-            Debug.Log("Player entered trigger");
+        {
+            arrowIndicator.playerEnteredTrigger2 = true;
+            arrowIndicator.playerEnteredTrigger3 = false;
+            Debug.Log("Player entered Gate 2 trigger");
             // Additional actions related to the player can be added here
+        }
+
+        if (other.CompareTag("Gate3"))
+        {
+            arrowIndicator.playerEnteredTrigger2 = false;
+            arrowIndicator.playerEnteredTrigger3 = true;
+            Debug.Log("Player entered Gate 3 trigger");
         }
     }
 }
--- Assets/Scripts/Arrow/ArrowIndicator.cs
+++ Assets/Scripts/Arrow/ArrowIndicator.cs
@@ -5,36 +5,42 @@ public class ArrowIndicator : MonoBehaviour
     public GameObject arrow1;
     public Transform goal1;
     public Transform goal2;
+    public Transform goal3;
     public float rotationSpeed;
 
-    public bool playerEnteredTrigger; 
+    public bool playerEnteredTrigger2;
+    public bool playerEnteredTrigger3;
 
     private void Update()
     {
-        if (playerEnteredTrigger)
+        if (playerEnteredTrigger2)
         {
             Debug.Log("Player entered trigger - Rotating towards Goal 2");
             RotateTowardsGoal2();
         }
-        else if (!playerEnteredTrigger) 
+        else if (playerEnteredTrigger3)
+        {
+            Debug.Log("Player entered trigger - Rotating towards Goal 3");
+            RotateTowardsGoal3();
+        }
+        else
         {
             Debug.Log("Player not in trigger - Rotating towards Goal 1");
             RotateTowardsGoal1();
         }
     }
 
-
-
-
     public void RotateTowardsGoal1()
     {
-        
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(goal1.position - transform.position), rotationSpeed * Time.deltaTime);
     }
     public void RotateTowardsGoal2()
     {
-        
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(goal2.position - transform.position), rotationSpeed * Time.deltaTime);
+    }
 
+    public void RotateTowardsGoal3()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(goal3.position - transform.position), rotationSpeed * Time.deltaTime);
     }
 }
