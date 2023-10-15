d836fa0e0f9040156a24f42729b4d70be701996c
MatHartley_HomePC <115046381+MatHartley@users.noreply.github.com>
2023-10-15 12:47:12 +1100

    15/10/23
    Edited direction arrow to show every gate, rather than just ranges

--- Assets/Scripts/Arrow/ArrowChange.cs
+++ Assets/Scripts/Arrow/ArrowChange.cs
@@ -5,20 +5,51 @@ public class ArrowChange : MonoBehaviour
     public ArrowIndicator arrowIndicator;
 
     public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Gate2"))
+    { 
+        if (other.CompareTag("Gate1"))
         {
-            arrowIndicator.playerEnteredTrigger2 = true;
-            arrowIndicator.playerEnteredTrigger3 = false;
-            Debug.Log("Player entered Gate 2 trigger");
-            // Additional actions related to the player can be added here
+            arrowIndicator.activeTrigger = 1;
+            Debug.Log("Player entered Gate 1 trigger");
         }
-
-        if (other.CompareTag("Gate3"))
+        else if (other.CompareTag("Gate2"))
         {
-            arrowIndicator.playerEnteredTrigger2 = false;
-            arrowIndicator.playerEnteredTrigger3 = true;
+            arrowIndicator.activeTrigger = 2;
+            Debug.Log("Player entered Gate 2 trigger");
+        }
+        else if (other.CompareTag("Gate3"))
+        {
+            arrowIndicator.activeTrigger = 3;
             Debug.Log("Player entered Gate 3 trigger");
+        }
+        else if (other.CompareTag("Gate4"))
+        {
+            arrowIndicator.activeTrigger = 4;
+            Debug.Log("Player entered Gate 4 trigger");
+        }
+        else if (other.CompareTag("Gate5"))
+        {
+            arrowIndicator.activeTrigger = 5;
+            Debug.Log("Player entered Gate 5 trigger");
+        }
+        else if (other.CompareTag("Gate6"))
+        {
+            arrowIndicator.activeTrigger = 6;
+            Debug.Log("Player entered Gate 6 trigger");
+        }
+        else if (other.CompareTag("Gate7"))
+        {
+            arrowIndicator.activeTrigger = 7;
+            Debug.Log("Player entered Gate 7 trigger");
+        }
+        else if (other.CompareTag("Gate8"))
+        {
+            arrowIndicator.activeTrigger = 8;
+            Debug.Log("Player entered Gate 8 trigger");
+        }
+        else if (other.CompareTag("Gate9"))
+        {
+            arrowIndicator.activeTrigger = 9;
+            Debug.Log("Player entered Gate 9 trigger");
         }
     }
 }
--- Assets/Scripts/Arrow/ArrowIndicator.cs
+++ Assets/Scripts/Arrow/ArrowIndicator.cs
@@ -2,45 +2,45 @@ using UnityEngine;
 
 public class ArrowIndicator : MonoBehaviour
 {
+    [Header("Arrow")]
     public GameObject arrow1;
+
+    [Header("Target Gates")]
     public Transform goal1;
     public Transform goal2;
     public Transform goal3;
-    public float rotationSpeed;
+    public Transform goal4;
+    public Transform goal5;
+    public Transform goal6;
+    public Transform goal7;
+    public Transform goal8;
+    public Transform goal9;
+    public Transform goal10;
 
-    public bool playerEnteredTrigger2;
-    public bool playerEnteredTrigger3;
+    [Header("Variables")]
+    public float rotationSpeed;
+    public int activeTrigger = 0;
 
     private void Update()
     {
-        if (playerEnteredTrigger2)
+        switch (activeTrigger)
         {
-            Debug.Log("Player entered trigger - Rotating towards Goal 2");
-            RotateTowardsGoal2();
-        }
-        else if (playerEnteredTrigger3)
-        {
-            Debug.Log("Player entered trigger - Rotating towards Goal 3");
-            RotateTowardsGoal3();
-        }
-        else
-        {
-            Debug.Log("Player not in trigger - Rotating towards Goal 1");
-            RotateTowardsGoal1();
+            default: RotateTowardsGoal(goal10); Debug.Log("Rotating towards Goal 10"); break;
+            case 0: RotateTowardsGoal(goal1); Debug.Log("Rotating towards Goal 1"); break;
+            case 1: RotateTowardsGoal(goal2); Debug.Log("Rotating towards Goal 2"); break;
+            case 2: RotateTowardsGoal(goal3); Debug.Log("Rotating towards Goal 3"); break;
+            case 3: RotateTowardsGoal(goal4); Debug.Log("Rotating towards Goal 4"); break;
+            case 4: RotateTowardsGoal(goal5); Debug.Log("Rotating towards Goal 5"); break;
+            case 5: RotateTowardsGoal(goal6); Debug.Log("Rotating towards Goal 6"); break;
+            case 6: RotateTowardsGoal(goal7); Debug.Log("Rotating towards Goal 7"); break;
+            case 7: RotateTowardsGoal(goal8); Debug.Log("Rotating towards Goal 8"); break;
+            case 8: RotateTowardsGoal(goal9); Debug.Log("Rotating towards Goal 9"); break;
+            case 9: RotateTowardsGoal(goal10); Debug.Log("Rotating towards Goal 10"); break;
         }
     }
 
-    public void RotateTowardsGoal1()
+    public void RotateTowardsGoal(Transform activeGoal)
     {
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(goal1.position - transform.position), rotationSpeed * Time.deltaTime);
-    }
-    public void RotateTowardsGoal2()
-    {
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(goal2.position - transform.position), rotationSpeed * Time.deltaTime);
-    }
-
-    public void RotateTowardsGoal3()
-    {
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(goal3.position - transform.position), rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(activeGoal.position - transform.position), rotationSpeed * Time.deltaTime);
     }
 }
